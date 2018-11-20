8959180fde47c3c7f4ed2bd05886f2b44ea23413
DilongFa <dilongfa@gmail.com>
2018-11-20 20:27:59 +0700

    Add some new features

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { inspect, format } = require('util')
 const allows = []
 const ignores = []
 const colors = [2, 3, 4, 5, 6]
+
 let instanceCounter = 0
 
 function ms(ms) {
@@ -28,7 +29,7 @@ function getState(ns) {
 
     for (let elm of ignores) {
         if (elm.test(ns)) return false
-    }
+    } 
 
     for (let elm of allows) {
         if (elm.test(ns)) return true
@@ -39,6 +40,7 @@ function getState(ns) {
 
 function start(ns = '') {
     if (ns === '') ns = process.env.DEBUG
+
     const ary = ns.split(/[\s,]+/)
     for (let elm of ary) { 
         if (!elm) continue
@@ -51,50 +53,57 @@ function start(ns = '') {
     }
 }
 
-function Debug(namespace) {
-    this.namespace = namespace
-    this.color = getColor()
-    this.previous = null
-    this.enabled = getState(namespace)
-
-    return this.debug.bind(this)
-}
-
-Debug.prototype.debug = function(...args) {
-   if (!this.enabled || args.length === 0) return
-    
-    const c = this.color
-    const now = Date.now()
-    const duration = now - (this.previous || now)
-    this.previous = now 
-
-    // Parse and reformat messages
-    if (typeof args[0] !== 'string') {
-        args.unshift('%s')
-    } else {
-        args[0] = args[0].replace(/%([oO])/g, '%s')
-    }
-
-    for(let i = 1; i < args.length; i++) {
-        switch(typeof args[i]) {
-            case 'function':
-                args[i] = args[i].toString()
-                break
-                case 'object':
-                args[i] = (args[i] instanceof Error) ? args[i].stack.replace(/   at /g, '@ ') : inspect(args[i], false, null, true)
-            break
-        }
-    }
-
-    const prefix = `\u001b[3${c};1m${this.namespace} \u001b[0m`
-    let str = `  ${prefix}`
-        str += format(...args).split('\n').join('\n  ' + prefix)
-        str += ` \u001b[3${c}m+` + ms(duration) + `\u001b[0m`
-
-    // Output
-    process.stderr.write(str + '\n')
-}
-
 start()
 
-module.exports = (namespace) => new Debug(namespace)
+module.exports = exports = (namespace, color) => {
+    let previous
+ 
+    debug.namespace = namespace
+    debug.color = color ? color : getColor()
+    debug.enabled = getState(namespace)
+    debug.sub = sub
+
+    function debug(...args) {
+        if (!debug.enabled) return
+        
+        const self = debug
+
+        const c = self.color
+        const now = Date.now()
+        const duration = now - (previous || now)
+        previous = now 
+    
+        // Parse and reformat messages
+        if (typeof args[0] !== 'string') {
+            args.unshift('%s')
+        } else {
+            args[0] = args[0].replace(/%([oO])/g, '%s')
+        }
+    
+        for(let i = 1; i < args.length; i++) {
+            switch(typeof args[i]) {
+                case 'function':
+                    args[i] = args[i].toString()
+                break
+                case 'object':
+                    args[i] = (args[i] instanceof Error) ? args[i].stack || args[i].message : inspect(args[i], false, null, true)
+                break
+            }
+        }
+    
+        const prefix = `\u001b[3${c};1m${self.namespace} \u001b[0m`
+        let str = `  ${prefix}`
+            str += format(...args).split('\n').join('\n  ' + prefix)
+            str += ` \u001b[3${c}m+` + ms(duration) + `\u001b[0m`
+    
+        process.stderr.write(str + '\n')
+    }
+
+    return debug
+}
+
+//Support colors: white|grey|black|blue|cyan|green|magenta|red|yellow
+function sub(namespace, color = '') {
+    color = (color === '') ? '3'+this.color : inspect.colors[color][0]
+    return exports(`${this.namespace}\u001b[${color};1m:${namespace}\u001b[0m`, this.color);
+}
